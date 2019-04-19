5f85b13cb3abe84c99ff5e813052c7fbd3d1f342
Nicolas Cueva <38636119+nicolascueva@users.noreply.github.com>
2019-04-18 21:02:47 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,30 @@
+import nltk
+import urllib.request
+#nltk.download()
+from nltk.corpus import stopwords
+import matplotlib
+
+response = urllib.request.urlopen('https://en.wikipedia.org/wiki/SpaceX')
+html = response.read()
+#print(html)
+
+from bs4 import BeautifulSoup
+import html5lib
+soup = BeautifulSoup(html,'html5lib')
+text = soup.get_text(strip = True)
+#print(text)
+
+tokens = [t for t in text.split()]
+#print(tokens)
+
+sr= stopwords.words('english')
+clean_tokens = tokens[:]
+for token in tokens:
+    if token in stopwords.words('english'):
+        clean_tokens.remove(token)
+
+freq = nltk.FreqDist(clean_tokens)
+for key,val in freq.items():
+    print(str(key) + ':' + str(val))
+
+freq.plot(20, cumulative=False)
